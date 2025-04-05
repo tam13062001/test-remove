70336d17f5bc558819fe308329bd582fa0502e17
tam13062001 <dtam768@gmail.com>
2025-04-05 19:08:05 +0700

    Solution Page

---
+++ theme/template-parts/content/slide-building.php
@@ -0,0 +1,38 @@
+<div class="py-[100px] mx-auto px-4 xl:px-0">
+    <h1 class="container text-4xl xl:text-5xl font-bold text-gray-900 mb-16">
+        Building from the ground up
+    </h1>
+    
+    <div class="flex gap-4 xl:gap-8">
+
+        
+        <div class="bg-white  flex border border-gray-300">
+            <p class="text-gray-600 p-8 mt-4">
+                Improves security and compliance
+            </p>
+            <img class="w-[324px] h-[324px] object-cover" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/slide1.png' ?>" alt="Data & AI" />
+
+            
+        </div>
+        
+        
+        <div class="bg-white  flex border border-gray-300">
+            <p class="text-gray-600 p-8 mt-4">
+            Increased development velocity
+            </p>
+            <img class="w-[324px] h-[324px] object-cover" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/slide2.png' ?>" alt="Data & AI" />
+
+            
+        </div>
+        
+        
+        <div class="bg-white  flex border border-gray-300">
+            <p class="text-gray-600 p-8 mt-4">
+            Enhanced operational efficiency
+            </p>
+            <img class="w-[324px] h-[324px] object-cover" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/slide3.png' ?>" alt="Data & AI" />
+
+            
+        </div>
+    </div>
+</div>
