f4d1f102f9ef71e338649d3f96aaa84ce0daf718
dev <dev>
2025-04-07 13:12:06 +0700

    support 2level menu

--- theme/template-parts/header/menu.php
+++ theme/template-parts/header/menu.php
@@ -1,27 +1,25 @@
-<!-- TODO load dynamic from config -->
+
 <?php $primary_menu_items = get_menu_tree('primary-menu'); ?>
 
 <div class="hidden lg:flex font-bold">
-    <?php
-    foreach ($primary_menu_items as $menu) {
-        $has_children = !empty($menu['children']);
-    ?>
-        <div class="menu-nav-item relative">
+    <?php foreach ($primary_menu_items as $menu) : ?>
+        <div class="group relative">
             <a class="px-4" href="<?php echo $menu['url'];?>">
                 <?php echo $menu['title']; ?>
             </a>
+            <?php if (!empty($menu['children'])) { ?>
+                <ul class="absolute bottom-0 list-none z-20 w-max m-0 top-[25px] hidden group-hover:block">
+                    <?php foreach ($menu['children'] as $submenu) : ?>
+                        <li class="relative group  bg-secondary hover:bg-secondary/80">
+                            <a href="<?php echo $submenu['url'] ?>" class="block px-4 py-2 font-bold">
+                                <?php echo $submenu['title'] ?>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php } ?>
         </div>
-    <?php } ?>
+    <?php endforeach; ?>
 
-<!--    <div class="px-4">-->
-<!--        <div class="border-b-2 border-white">-->
-<!--            Our services-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div class="px-4">Our solutions</div>-->
-<!--    <div class="px-4">Industry</div>-->
-<!--    <div class="px-4">Insights</div>-->
-<!--    <div class="px-4">Successful stories</div>-->
-<!--    <div class="px-4">About us</div>-->
-<!--    <div class="px-4">Career</div>-->
+
 </div>
