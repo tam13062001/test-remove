5b3f331f30bf7e6f7ca5621881c2b3e245a5abfa
dev <dev>
2025-04-07 13:12:06 +0700

    support 2level menu

--- theme/template-parts/header/header.php
+++ theme/template-parts/header/header.php
@@ -1,7 +1,9 @@
 <header class="absolute w-full z-[9999]">
     <div class="container">
         <div class="flex py-[32px] items-center justify-between text-[16px] text-white">
-            <?php get_template_part('template-parts/content/logo'); ?>
+            <a href="/">
+                <?php get_template_part('template-parts/content/logo'); ?>
+            </a>
             <?php get_template_part('template-parts/header/menu'); ?>
             <?php get_template_part('template-parts/content/language-switcher'); ?>
         </div>
