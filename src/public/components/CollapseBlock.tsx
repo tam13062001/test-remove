7930ddf420f2b93137ee469d3ab20c703c68c8c8
dev <dev>
2025-04-07 14:01:29 +0700

    support faq

---
+++ src/public/components/CollapseBlock.tsx
@@ -0,0 +1,28 @@
+import {Collapse} from "antd";
+import {BaseProps} from "../core/get-props";
+import {ArrowRightOutlined} from "@ant-design/icons";
+
+
+type CollapseBlockProps = {
+  title: string
+  content: string
+}
+
+export default function CollapseBlock(props: BaseProps<CollapseBlockProps>) {
+  const { title, content } = props.data
+  return (
+    <Collapse
+      expandIconPosition={'end'}
+      ghost
+      expandIcon={({ isActive }) => <div className={'!text-primary'}><ArrowRightOutlined color={'#315cd4'} rotate={isActive ? 90 : 0} /></div>}
+    >
+      <Collapse.Panel key={'1'} header={(
+        <div className={'font-[700] text-primary '}>
+          {title}
+        </div>
+      )} >
+        { content }
+      </Collapse.Panel>
+    </Collapse>
+  )
+}
