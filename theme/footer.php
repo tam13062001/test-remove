efc30cc464114bf8f49fd1ac0cbc3246a682d933
dev <dev>
2025-04-12 18:20:24 +0700

    update

--- theme/footer.php
+++ theme/footer.php
@@ -5,16 +5,7 @@
                     <img class="w-[142px]" alt="" src="<?php echo get_stylesheet_directory_uri().'/assets/images/logo.png' ?>" />
                 </div>
                 <div class="w-full lg:w-2/5 space-y-6">
-                    <div class="grid grid-cols-2 gap-4">
-                        <div class="font-[700] text-[16px]">Our services</div>
-                        <div class="font-[700] text-[16px]">Successful stories</div>
-                        <div class="font-[700] text-[16px]">Our solutions</div>
-                        <div class="font-[700] text-[16px]">About us</div>
-                        <!-- <div class="font-[700] text-[16px]">Industry</div>
-                        <div class="font-[700] text-[16px]">Career</div>
-                        <div class="font-[700] text-[16px]">Insights</div> -->
-                    </div>
-
+                    <?php get_template_part('template-parts/footer/menu'); ?>
                 </div>
                 <div class="lg:w-1/5">
                     <div class="font-[700] text-[16px] mb-4">Offices</div>
@@ -26,30 +17,14 @@
                     </div>
                 </div>
                 <div class="w-1/5">
-                    <!-- <div class="flex space-x-4">
-                        <a class="shrink-0" href="#">
-                            <img class="w-9 h-9" src="<?php echo get_assets_from_path('icons/linkedin.png');?>" alt="" />
-                        </a>
-                        <a class="shrink-0" href="#">
-                            <img class="w-9 h-9" src="<?php echo get_assets_from_path('icons/facebook.png');?>" alt="" />
-                        </a>
-                        <a class="shrink-0" href="#">
-                            <img class="w-9 h-9" src="<?php echo get_assets_from_path('icons/x.png');?>" alt="" />
-                        </a>
-                    </div> -->
+<!--                    --><?php //get_template_part('template-parts/footer/social-network'); ?>
                 </div>
             </div>
 
             <div class="flex flex-col lg:flex-row lg:mb-[100px] space-y-9 lg:space-y-0">
                 <div class="w-1/5"> </div>
                 <div class="flex flex-row lg:justify-start  py-0  box-border max-w-full">
-                    <div class="lg:space-x-[50px] text-[16px] lg:flex">
-                        <div class="space-x-[50px] mb-9 lg:mb-0">
-                            <a href="/term-of-use/">Term of use</a>
-                            <a href="/privacy/">Privacy statement</a>
-                        </div>
-                        <a href="#">Copyright © 2025 by Datum</a>
-                    </div>
+                    <?php get_template_part('template-parts/footer/privacy-menu'); ?>
                 </div>
             </div>
             
--- theme/functions.php
+++ theme/functions.php
@@ -3,14 +3,23 @@
 if ( ! defined( 'WP_DEVELOPMENT_MODE' ) ) {
     define( 'WP_DEVELOPMENT_MODE', 'all' );
 }
-
 require_once get_template_directory() . '/inc/autoload.php';
 
-$wp_theme = wp_get_theme();
 global $rocket;
+
+$wp_theme = wp_get_theme();
+$theme_name = $wp_theme->get( 'Name' );
+
 $rocket = new RocketTheme(array(
     'version' => $wp_theme->get('Version'),
-    'name' => $wp_theme->get('Theme Name'),
+    'name' => $theme_name,
     'development_mode' => true
 ));
 $rocket->load();
+
+$rocket->add_admin_menu(array(
+    'name' => $theme_name,
+    'title' => $theme_name,
+    'slug' => 'datum',
+    'icon_url' => get_assets_from_path('/icons/datum.svg'),
+));
