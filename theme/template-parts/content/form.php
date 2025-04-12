0bc9cc3087c31ff91b10d51ab9d0d58bd69f40e8
tam13062001 <dtam768@gmail.com>
2025-04-12 11:03:26 +0700

    fix form contact

--- theme/template-parts/content/form.php
+++ theme/template-parts/content/form.php
@@ -17,14 +17,14 @@ $image_url = $args['image_url'] ?? '';
             <!-- using form component to handle form submit, validate and others from events -->
             <div class="w-full lg:w-1/2">
                 <div class="flex flex-col gap-4 text-[20px] lg:w-[555px]">
-                    <input type="text" name="firstName" placeholder="First Name*" required class="border border-black p-2 ">
-                    <input type="text" name="lastName" placeholder="Last Name*" required class="border border-black p-2 ">
-                    <input type="email" name="email" placeholder="Email*" required class="border border-black p-2 ">
-                    <input type="text" name="jobTitle" placeholder="Job Title*" required class="border border-black p-2 ">
-                    <input type="tel" name="phoneNumber" placeholder="Phone Number*" required class="border border-black p-2 ">
-                    <input type="text" name="company" placeholder="Company/Organization*" required class="border border-black p-2 ">
+                    <input type="text" name="firstName" placeholder="First Name*" required class="border border-gray-300 p-2  placeholder:text-black">
+                    <input type="text" name="lastName" placeholder="Last Name*" required class="border border-gray-300 p-2 placeholder:text-black">
+                    <input type="email" name="email" placeholder="Email*" required class="border border-gray-300 p-2 placeholder:text-black">
+                    <input type="text" name="jobTitle" placeholder="Job Title*" required class="border border-gray-300 p-2 placeholder:text-black">
+                    <input type="tel" name="phoneNumber" placeholder="Phone Number*" required class="border border-gray-300 p-2 placeholder:text-black">
+                    <input type="text" name="company" placeholder="Company/Organization*" required class="border border-gray-300 p-2 placeholder:text-black">
 
-                    <select name="country" required class="border border-black p-2 ">
+                    <select name="country" required class="border border-gray-300 p-2 ">
                         <option value="">Country*</option>
                         <option value="us">United States</option>
                         <option value="vn">Vietnam</option>
@@ -32,11 +32,11 @@ $image_url = $args['image_url'] ?? '';
 
                     </select>
 
-                    <textarea name="message" placeholder="Your Message*" required class="border border-black p-2 h-24"></textarea>
+                    <textarea name="message" placeholder="Your Message*" required class="border border-gray-300 p-2 h-24 placeholder:text-black"></textarea>
 
                     <div class="flex items-start gap-2">
-                        <input type="checkbox" name="consent" required>
-                        <label class="text-xs">I agree to Datum collecting and processing my personal data to allow me to receive information on Datum services.</label>
+                        <input type="checkbox" name="consent" required class="w-4 h-4 lg:mt-1" />
+                        <label class="text-[16px] font-regular">I agree to Datum collecting and processing my personal data to allow me to receive information on Datum services.</label>
                     </div>
                 </div>
             </div>
