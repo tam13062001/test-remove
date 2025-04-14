94d0b0cc3ca6a6259f9da1524d6411fbccfc1e6c
tam13062001 <dtam768@gmail.com>
2025-04-14 21:20:09 +0700

    newest

--- theme/template-parts/content/info-section-with-image-reverse-lesson.php
+++ theme/template-parts/content/info-section-with-image-reverse-lesson.php
@@ -13,7 +13,7 @@ $image_url = $args['image_url'] ?? '';
     <div class="container pt-[40px] lg:pt-0 z-10 relative">
             
     <div class="relative lg:absolute top-0 right-0 lg:pt-[100px] lg:w-3/5 bg-white pt-6 lg:pt-5 lg:pl-[100px]">
-            <div class="lg:w-[70%] font-bold text-[32px] lg:text-[40px] lg:mb-[10px] lg:mt-[130px]">
+            <div class="lg:w-[70%] font-bold text-[32px] lg:text-[40px] lg:mb-[10px] ">
                 <h2>
                     <?php echo $title ?>
                 </h2>
--- theme/template-parts/content/info-section-with-image-story-percent-l.php
+++ theme/template-parts/content/info-section-with-image-story-percent-l.php
@@ -9,49 +9,44 @@ $image_url = $args['image_url'] ?? '';
 
 <div class="relative lg:min-h-[880px] lg:my-[88px] lg:mt-[100px] lg:mb-[150px]">
 
-
-
+    <!-- Text content section -->
     <div class="relative lg:absolute top-0 right-0 lg:w-3/5 bg-white px-12 lg:px-0 pt-6 lg:pt-5 lg:pl-[60px]">
-            <div class="lg:w-[70%] font-bold text-[32px] lg:text-[40px] lg:mb-[10px] ">
-                <h2>
-                    <?php echo $title ?>
-                </h2>
-            </div> 
-            <div>
-                <div class="lg:w-[80%] text-primary text-[16px] lg:text-[32px] font-normal leading-[170%]">
-                    <?php echo $heading ?>
-                </div>
+        <div class="lg:w-[70%] font-bold text-[32px] lg:text-[40px] lg:mb-[10px]">
+            <h2><?php echo $title; ?></h2>
+        </div>
+
+        <div class="lg:w-[80%] text-primary text-[16px] lg:text-[32px] font-normal leading-[170%]">
+            <?php echo $heading; ?>
+        </div>
+
+        <div class="bg-white pt-6 lg:py-5 lg:pr-[100px]">
+            <div class="text-[16px] lg:text-xl font-normal leading-[170%]">
+                <?php echo $content; ?>
             </div>
-            
-            <div class=" bg-white pt-6 lg:py-5 lg:pr-[100px]">
-                <div class="text-[16px] lg:text-xl font-normal leading-[170%]">
-                    <?php echo $content; ?>
-                </div>
 
-            <div class=" [&>:not(:last-child)]:border-b ">
+            <div class="[&>:not(:last-child)]:border-b">
 
-                <div class="border-primary lg:py-5 flex flex-col lg:flex-row lg:space-x-5"></div>
-                <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-6 border-y-1 border-primary">
+                <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-6 border-y border-primary lg:py-5">
                     <div class="flex flex-col lg:flex-row items-center relative">
-                        <div class="relative text-[56px] lg:text-[96px] leading-[170%] mr-0 lg:mr-5 
+                        <div class="relative text-[56px] lg:text-[96px] leading-[170%] mr-0 lg:mr-5 inline-block 
                             bg-gradient-to-r from-secondary to-primary text-transparent bg-clip-text">
                             <?php echo $percent; ?>
-                            <!-- <div class="absolute h-2 w-[60px] bg-primary bottom-0 left-0"></div> -->
                         </div>
-                        <div class="flex leading-[170%]  text-[20px] lg:text-2xl text-primary font-bold mb-5 lg:mb-0">
-                            <?php echo $percent_content; ?>
+                        <div class="flex leading-[170%] text-[20px] lg:text-2xl text-primary font-bold mb-5 lg:mb-0">
+                            <strong><?php echo $percent_content; ?></strong>
                         </div>
                     </div>
                 </div>
-                <div class="border-primary border-t-1 lg:py-5 flex flex-col lg:flex-row lg:space-x-5"></div>
+
             </div>
-
         </div>
     </div>
 
-    <div class="lg:w-3/5 pt-[40px] lg:py-0 lg:absolute left-0 top-0 h-full overflow-hidden ">
+    <!-- Image section -->
+    <div class="lg:w-3/5 pt-[40px] lg:py-0 lg:absolute left-0 top-0 h-full overflow-hidden">
         <div class="flex items-end h-full">
-            <img class="object-cover h-auto lg:h-[636px] w-full lg:w-[96%]" src="<?php echo $image_url?>" alt="" />
+            <img class="object-cover h-auto lg:h-[636px] w-full lg:w-[96%]" src="<?php echo $image_url; ?>" alt="" />
         </div>
     </div>
+
 </div>
