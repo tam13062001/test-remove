983c29e509ee9dffbdc112370544f4d7d9e42d06
dev <dev>
2025-06-19 07:47:26 +0700

    update about partners

--- theme/template-parts/content/partners.php
+++ theme/template-parts/content/partners.php
@@ -6,11 +6,11 @@ $bg_image_url = $args['bg_image_url'] ?? '';
 ?>
 
 <div class="relative bg-gradient-to-r from-secondary to-primary">
-    <img class="absolute top-0 h-full right-0" src="<?php echo get_assets_from_path('images/partners_bg.png') ?>" alt="" />
-    <div class="container py-10 lg:py-[120px] z-10 relative">
-        <div class="flex flex-col-reverse lg:flex-row lg:gap-20 ">
-            <div class="lg:w-1/2 text-white">
-                <h2 class="font-[700] text-[32px] lg:text-[40px] mb-6 lg:mb-12 lg:ml-12 lg:mt-6">
+    <img class="absolute top-0 h-full right-0 object-cover" src="<?php echo get_assets_from_path('images/partners_bg.png') ?>" alt="" />
+    <div class="container py-10 lg:py-[120px] xl:py-[190px] z-10 relative">
+        <div class="flex flex-col-reverse lg:flex-row lg:gap-20 w-full">
+            <div class="lg:w-1/2 text-white flex flex-col justify-center">
+                <h2 class="font-[700] text-[28px] md:text-[32px] lg:text-[36px] xl:text-[40px] mb-6 lg:mb-12 lg:ml-12 lg:mt-6">
                     Our Partners
                 </h2>
                 <div class="text-[16px] lg:ml-12 lg:text-[20px] font-Regular leading-[170%] mb-6 lg:mb-12">
@@ -18,8 +18,19 @@ $bg_image_url = $args['bg_image_url'] ?? '';
                     ensure that our technology solutions are seamlessly integrated with your business strategy, creating a comprehensive roadmap that paves the way for sustainable growth and success.
                 </div>
             </div>
-            <div class="py-10 lg:py-0 lg:w-1/2 lg:mb-20 ">
-                <img class="inline-block w-[200px] h-[200px] lg:w-[450px] lg:h-[450px] sm:ml-8 " src="<?php echo get_assets_from_path('images/aws-partner1.png') ?>" alt="" />
+            <div class="py-10 lg:py-0 lg:w-1/2 lg:mb-20 flex flex-col items-center">
+
+                <div class="flex justify-center gap-4">
+                    <img class="w-[140px] h-[140px]  lg:w-[391px] lg:h-[391px] -mt-[30px] lg:-mt-[60px]"
+                         src="<?php echo get_assets_from_path('images/aws-partner-1.png') ?>" alt="Partner 1" />
+                    <img class="w-[100px] h-[100px]  lg:w-[277px] lg:h-[277px]  lg:-ml-[50px] lg:mt-[25px] -mt-[2px] -ml-[30px]"
+                         src="<?php echo get_assets_from_path('images/aws-partner-2.png') ?>" alt="Partner 2" />
+                </div>
+
+                <div class="mt-[-33px] md:mt-[-70px] lg:mt-[-90px] mt-[-35px] ml-[37px] md:ml-[70px] lg:ml-[110px]">
+                    <img class="w-[100px] h-[100px]  lg:w-[277px] lg:h-[277px] "
+                         src="<?php echo get_assets_from_path('images/aws-partner-3.png') ?>" alt="Partner 3" />
+                </div>
             </div>
         </div>
     </div>
