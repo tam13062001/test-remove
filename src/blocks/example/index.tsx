ea7762a9c12bfbfd8b3e4c4ad43c8fd8bfbb1154
dev <dev>
2025-07-10 06:37:03 +0700

    remove example block

--- src/blocks/example/index.tsx
+++
@@ -1,8 +0,0 @@
-import Edit from './edit'
-import metadata from './block.json'
-import {registerDynamicBlock} from '../_shared/tools/registerDynamicBlock'
-
-registerDynamicBlock(metadata, {
-  edit: Edit
-})
-
